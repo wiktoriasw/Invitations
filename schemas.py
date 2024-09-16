4b6574857a619f39686717889a7354a9d27bebaa
Wiktoria <wiktoriaswiech.ws@gmail.com>
2024-09-16 19:17:39 +0200

    Update schemas

--- schemas.py
+++ schemas.py
@@ -1,25 +1,29 @@
 from pydantic import BaseModel
+from datetime import datetime
 
+## NIEDZ 15.09
 
-class ItemBase(BaseModel):
-    title: str
+class EventBase(BaseModel):
+    name: str
     description: str | None = None
+    start_time: datetime
+    location: str
+    menu: str
+    decision_deadline: datetime
+    organizer_id: int
 
 
-class ItemCreate(ItemBase):
+class EventCreate(EventBase):
     pass
 
-
-class Item(ItemBase):
-    id: int
-    owner_id: int
+class Event(EventBase):
+    event_id: int
 
     class Config:
         orm_mode = True
 
-
 class UserBase(BaseModel):
-    email: str
+    login: str
 
 
 class UserCreate(UserBase):
@@ -27,9 +31,31 @@ class UserCreate(UserBase):
 
 
 class User(UserBase):
-    id: int
-    is_active: bool
-    items: list[Item] = []
+    user_id: int
+    events: list[Event] = []
+
+    class Config:
+        orm_mode = True
+
+
+class GuestBase(BaseModel):
+    guest_id: int
+    event_id: int
+    name: str 
+    surname: str 
+    email: str
+    phone: str
+    answer: bool
+    menu: str
+    comments: str
+
+
+class GuestCreate(GuestBase):
+    pass
+
+
+class Guest(GuestBase):
+    guest_id: int
 
     class Config:
         orm_mode = True

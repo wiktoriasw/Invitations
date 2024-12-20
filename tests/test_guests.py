496ae4bed19cc289472074fec272235296b82bbb
wiktoriasw <wiktoriaswiech.ws@gmail.com>
2024-12-20 18:31:26 +0100

    Add tests for guests

--- tests/test_guests.py
+++ tests/test_guests.py
@@ -20,7 +20,16 @@ event_1 = {
     "start_time": "2024-12-05T14:00:00",
     "location": "Warszawa",
     "menu": "Wegetariańskie;Mięsne",
-    "decision_deadline": "2024-11-28T12:00:00",
+    "decision_deadline": "2020-11-28T12:00:00",
+}
+
+event_2 = {
+    "name": "Kolacja \u015awi\u0105teczna Firmowa",
+    "description": "Wigilijne spotkanie z zespo\u0142em",
+    "start_time": "2024-12-20T18:00:00",
+    "location": "Krak\u00f3w",
+    "menu": "polskie;wloskie",
+    "decision_deadline": "2050-11-28T12:00:00",
 }
 
 guest_1 = {
@@ -28,6 +37,17 @@ guest_1 = {
     "surname": "Kowalska",
     "email": "karkowal@gmail.com",
     "phone": "+48765456384",
+    "has_companion": True,
+}
+
+guest_answer_1 = {
+    "answer": True,
+    "menu": "Wegetariańskie",
+}
+
+guest_answer_2 = {
+    "answer": True,
+    "menu": "polskie",
 }
 
 
@@ -75,7 +95,7 @@ def test_delete_guest_from_event():
     assert response.status_code == 404
 
 
-def test_wrong_user_delete_guests():
+def test_different_user_delete_guests():
     test_utils.create_user(client=client, user=user_1)
     user1_token = test_utils.login_user(client=client, user=user_1)
     response = test_utils.create_event(client=client, event=event_1, token=user1_token)
@@ -111,7 +131,7 @@ def test_wrong_user_delete_guests():
     assert response.status_code == 200
 
 
-def test_wrong_user_add_guests():
+def test_different_user_add_guests():
     test_utils.create_user(client=client, user=user_1)
     user1_token = test_utils.login_user(client=client, user=user_1)
 
@@ -140,4 +160,125 @@ def test_wrong_user_add_guests():
         },
     )
 
+    assert response.status_code == 200
     assert len(response.json()) == 0
+
+
+def test_add_guest_to_event():
+    test_utils.create_user(client, user_1)
+    user1_token = test_utils.login_user(client, user_1)
+    response = test_utils.create_event(client, event_1, user1_token)
+
+    event_uuid = response.json()["uuid"]
+    assert event_uuid is not None
+    guest_1["event_uuid"] = event_uuid
+
+    response = client.post(
+        "/guests",
+        json=guest_1,
+        headers={
+            "Authorization": f"Bearer {user1_token}",
+        },
+    )
+
+    assert response.status_code == 200
+
+
+def test_different_user_add_guest_for_not_their_event():
+    test_utils.create_user(client, user_1)
+    user1_token = test_utils.login_user(client, user_1)
+
+    test_utils.create_user(client, user_2)
+    user2_token = test_utils.login_user(client, user_2)
+    response = test_utils.create_event(client, event_2, user2_token)
+
+    event_uuid = response.json()["uuid"]
+    assert event_uuid is not None
+    guest_1["event_uuid"] = event_uuid
+
+    response = client.post(
+        "/guests",
+        json=guest_1,
+        headers={
+            "Authorization": f"Bearer {user1_token}",
+        },
+    )
+
+    assert response.status_code == 401
+
+
+def test_get_guest_by_uuid_from_event():
+    test_utils.create_user(client, user_1)
+    user1_token = test_utils.login_user(client, user_1)
+    response = test_utils.create_event(client, event_1, user1_token)
+
+    event_uuid = response.json()["uuid"]
+    assert event_uuid is not None
+
+    guest_1["event_uuid"] = event_uuid
+
+    response = test_utils.add_guest_to_event(client, guest_1, user1_token)
+    guest_uuid = response.json()["uuid"]
+
+    response = client.get(
+        f"/guests/{guest_uuid}",
+    )
+
+    assert response.status_code == 200
+
+    response_data = response.json()
+    assert response_data["uuid"] == guest_uuid
+    assert response_data["name"] == guest_1["name"]
+
+
+def test_update_guest_answer_before_deadline():
+    test_utils.create_user(client, user_1)
+    user1_token = test_utils.login_user(client, user_1)
+    response = test_utils.create_event(client, event_2, user1_token)
+
+    event_uuid = response.json()["uuid"]
+    assert event_uuid is not None
+
+    event_decision_deadline = response.json()["decision_deadline"]
+    assert event_decision_deadline is not None
+
+    guest_1["event_uuid"] = event_uuid
+
+    response = test_utils.add_guest_to_event(client, guest_1, user1_token)
+    guest_uuid = response.json()["uuid"]
+
+    response = client.post(
+        f"/guests/{guest_uuid}/answer",
+        json=guest_answer_2,
+    )
+
+    assert response.status_code == 200
+
+    updated_answer = response.json()
+
+    assert updated_answer["menu"] == guest_answer_2["menu"]
+    assert updated_answer["answer"] == guest_answer_2["answer"]
+
+
+def test_guest_cannot_update_answer_after_deadline():
+    test_utils.create_user(client, user_1)
+    user1_token = test_utils.login_user(client, user_1)
+    response = test_utils.create_event(client, event_1, user1_token)
+
+    event_uuid = response.json()["uuid"]
+    assert event_uuid is not None
+
+    event_decision_deadline = response.json()["decision_deadline"]
+    assert event_decision_deadline is not None
+
+    guest_1["event_uuid"] = event_uuid
+
+    response = test_utils.add_guest_to_event(client, guest_1, user1_token)
+    guest_uuid = response.json()["uuid"]
+
+    response = client.post(
+        f"/guests/{guest_uuid}/answer",
+        json=guest_answer_1,
+    )
+
+    assert response.status_code == 406
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -46,3 +46,44 @@ def create_event(client: TestClient, event: Dict, token: str):
     assert response.json()["description"] == event["description"]
 
     return response
+
+
+def create_guest(client: TestClient, guest: Dict, event_uuid: str, token: str):
+
+    guest["event_uuid"] = event_uuid
+
+    response = client.post(
+        "/guests",
+        json=guest,
+        headers={
+            "Authorization": f"Bearer {token}",
+        },
+    )
+
+    assert response.status_code == 200
+
+    return response
+
+
+def update_guest_answer(client: TestClient, guest_answer: Dict):
+
+    response = client.post("f/{guest_uuid}/answer", json=guest_answer)
+
+    assert response.status_code == 200
+
+    return response
+
+
+def add_guest_to_event(client: TestClient, guest_1: Dict, token: str):
+
+    response = client.post(
+        "/guests",
+        json=guest_1,
+        headers={
+            "Authorization": f"Bearer {token}",
+        },
+    )
+
+    assert response.status_code == 200
+
+    return response
